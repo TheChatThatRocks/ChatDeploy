577ad6b4686c106881b354530c1db58fcf557934
mariuscrsn <mariuscrsn@gmail.com>
2020-05-24 01:59:00 +0200

    added links

---
+++ ec2_master_deploy.sh
@@ -0,0 +1,4 @@
+cd ~
+git clone https://1d28beb4ccf496b2625dc0967e67f9f332ce2fe4@github.com/TheChatThatRocks/ChatEncryptionAPI.git
+git clone https://1d28beb4ccf496b2625dc0967e67f9f332ce2fe4@github.com/TheChatThatRocks/ChatBackendAPI.git
+git clone https://1d28beb4ccf496b2625dc0967e67f9f332ce2fe4@github.com/TheChatThatRocks/ChatDeploy.git
