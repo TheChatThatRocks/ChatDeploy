aabd1465d771e9871d45f2652751164b8f9c8d7b
mariuscrsn <mariuscrsn@gmail.com>
2020-05-24 03:43:06 +0200

    update pull

--- ec2_master_deploy.sh
+++ ec2_master_deploy.sh
@@ -1,5 +1,8 @@
-sudo apt install -y awscli
-mkdir -p ~/.aws
+cd ~/ChatDeploy
+git pull
+
+#sudo apt install -y awscli
+#mkdir -p ~/.aws
 
 # ECR credentials
 cat <<EOT >> ~/.aws/credentials
@@ -9,7 +12,5 @@ aws_secret_access_key=3iNa434cLhuJcOqaPo5CCeguk7X7MXHpTsoYbNw4
 aws_session_token=FwoGZXIvYXdzECMaDFRiPKosvWr5Gp8ZQSK/Aeo7LDdIVq+/HIFACDlgzMocFiW0G4qPv3HFiiLPf7DymbzNGmTXI2lUsM0YSdEpDA0Xz+xd8KKWTiurDFBkrrC/PToPlkhKlCPq3r9VYfxGrBcjycEme7DZRky5NEDsxYXiJCtCT0cJTEocHaQQdeHJNlof2dH/AdPA029fye4cHHvdZTrTlUQ1gfYLtsEAIU5N6HQ3l/tUKihzgoI6vJYCywg4qPrILZh/zUdWNkHx2BBShFc2aqIbATUNpa3QKNGdp/YFMi34TlUFzbBouY42SrN5+DcOPWXWD4YQJnZu30vv7HwLheuaJJdwfEVOoSiLDrc=
 sudo $(aws ecr get-login --region us-east-1 | sed -e 's/-e none//g')
 
-cd ~/ChatDeploy
-git pull
 docker stack deploy -c docker-compose-aws.yml chat-stack
 # docker stack ps chat-stack
