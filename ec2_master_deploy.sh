cd7bdb43039a3bd3b9d11b1367408bcc5f650068
mariuscrsn <mariuscrsn@gmail.com>
2020-05-24 14:16:30 +0000

    asadasda

--- ec2_master_deploy.sh
+++ ec2_master_deploy.sh
@@ -1,3 +1,5 @@
+cd ~/ChatBackendApi/ && git pull
+cd ~/ChatEncryptionApi/ && git pull
 cd ~/ChatDeploy
 git pull
 
@@ -11,6 +13,9 @@ git pull
 #aws_session_token=FwoGZXIvYXdzECMaDFRiPKosvWr5Gp8ZQSK/Aeo7LDdIVq+/HIFACDlgzMocFiW0G4qPv3HFiiLPf7DymbzNGmTXI2lUsM0YSdEpDA0Xz+xd8KKWTiurDFBkrrC/PToPlkhKlCPq3r9VYfxGrBcjycEme7DZRky5NEDsxYXiJCtCT0cJTEocHaQQdeHJNlof2dH/AdPA029fye4cHHvdZTrTlUQ1gfYLtsEAIU5N6HQ3l/tUKihzgoI6vJYCywg4qPrILZh/zUdWNkHx2BBShFc2aqIbATUNpa3QKNGdp/YFMi34TlUFzbBouY42SrN5+DcOPWXWD4YQJnZu30vv7HwLheuaJJdwfEVOoSiLDrc=
 #" > ~/.aws/credentials
 sudo $(aws ecr get-login --region us-east-1 | sed -e 's/-e none//g')
+
+# Build images and upload to ECR
+#./push_images.sh
 
 sudo docker stack deploy -c docker-compose-aws.yml --with-registry-auth chat-stack
 
