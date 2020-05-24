ea5306256d7008c9e577350609361e2a9fb86c5b
mariuscrsn <mariuscrsn@gmail.com>
2020-05-24 03:23:55 +0200

    credentials aws

--- ec2_master_deploy.sh
+++ ec2_master_deploy.sh
@@ -1,4 +1,15 @@
-cd ~
-git clone https://1d28beb4ccf496b2625dc0967e67f9f332ce2fe4@github.com/TheChatThatRocks/ChatEncryptionAPI.git
-git clone https://1d28beb4ccf496b2625dc0967e67f9f332ce2fe4@github.com/TheChatThatRocks/ChatBackendAPI.git
-git clone https://1d28beb4ccf496b2625dc0967e67f9f332ce2fe4@github.com/TheChatThatRocks/ChatDeploy.git
+sudo apt install -y awscli
+mkdir -p ~/.aws
+
+# ECR credentials
+cat <<EOT >> ~/.aws/credentials
+[default]
+aws_access_key_id=ASIAUVB4H4CE7VBB44NS
+aws_secret_access_key=/ZR/LgngvtTMuJeF3aaBvhMli1WHUy3iLJQsb6Cq
+aws_session_token=FwoGZXIvYXdzECIaDEfC4w7G0AA5SeOhWCK/AWAh7escUlv92L1rJ+fA2mcHEGkZuTGPUdGGBGsQ1epjwXA1JlasQpVkZSCz4PrYxsQmyGUXhymtamM8JgIrY8etIQHUCQhrOg0Lq+1CRg9mu/lfV5mZwK1jBI19X5WnvlSgeoALeooCBfkJyX5YGaamX4lb8C+R93tQCtqanIY77MIebblIqbn6O/TzyXqnLhQfXmkRTsJQ26vJW7z2ebJAPkhrq
+sudo $(aws ecr get-login --region us-east-1 | sed -e 's/-e none//g')
+
+cd ~/ChatDeploy
+git pull
+docker stack deploy -c docker-compose-aws.yml chat-stack
+# docker stack ps chat-stack
