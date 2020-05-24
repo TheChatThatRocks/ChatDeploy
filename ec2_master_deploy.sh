d23359ce060ccb99c7b296ce3624f779adbc69c9
mariuscrsn <mariuscrsn@gmail.com>
2020-05-24 03:47:47 +0200

    new cred

--- ec2_master_deploy.sh
+++ ec2_master_deploy.sh
@@ -5,11 +5,11 @@ git pull
 #mkdir -p ~/.aws
 
 # ECR credentials
-cat <<EOT >> ~/.aws/credentials
-[default]
+echo "[default]
 aws_access_key_id=ASIAT7DKDJC4RQWVCNGF
 aws_secret_access_key=3iNa434cLhuJcOqaPo5CCeguk7X7MXHpTsoYbNw4
 aws_session_token=FwoGZXIvYXdzECMaDFRiPKosvWr5Gp8ZQSK/Aeo7LDdIVq+/HIFACDlgzMocFiW0G4qPv3HFiiLPf7DymbzNGmTXI2lUsM0YSdEpDA0Xz+xd8KKWTiurDFBkrrC/PToPlkhKlCPq3r9VYfxGrBcjycEme7DZRky5NEDsxYXiJCtCT0cJTEocHaQQdeHJNlof2dH/AdPA029fye4cHHvdZTrTlUQ1gfYLtsEAIU5N6HQ3l/tUKihzgoI6vJYCywg4qPrILZh/zUdWNkHx2BBShFc2aqIbATUNpa3QKNGdp/YFMi34TlUFzbBouY42SrN5+DcOPWXWD4YQJnZu30vv7HwLheuaJJdwfEVOoSiLDrc=
+" >> ~/.aws/credentials
 sudo $(aws ecr get-login --region us-east-1 | sed -e 's/-e none//g')
 
 docker stack deploy -c docker-compose-aws.yml chat-stack
